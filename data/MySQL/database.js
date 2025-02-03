e2f63c9f78c8c59151dce283d328fa25e4570463
NatesHonor <nathaniel.j.marcellus@gmail.com>
2025-02-03 14:07:40 -0500

    Addded acquisition logging

--- data/MySQL/database.js
+++ data/MySQL/database.js
@@ -3,12 +3,14 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.MySQL_HOST,
+  database: process.env.MySQL_DATABASE,
   user: process.env.MySQL_USERNAME,
   password: process.env.MySQL_PASSWORD,
-  database: process.env.MySQL_DATABASE,
 });
 
+
 pool.on('acquire', () => {
+  console.log("Acquired MySQL Connection")
 });
   
 
